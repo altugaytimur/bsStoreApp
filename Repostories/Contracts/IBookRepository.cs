d2ab3c36c78ac45f9675c54159e0acde138a464c
Altug Aytimur <aytimuraltug@gmail.com>
2024-04-05 17:53:25 +0300

    Services Extensions Created

--- Repostories/Contracts/IBookRepository.cs
+++ Repostories/Contracts/IBookRepository.cs
@@ -10,7 +10,7 @@ namespace Repostories.Contracts
     public interface IBookRepository:IRepositoryBase<Book>
     {
         IQueryable<Book> GetAllBooks(bool trackChanges);
-        IQueryable<Book> GetOneBooksById(int id,bool trackChanges);
+        Book GetOneBooksById(int id,bool trackChanges);
         void CreateOneBook(Book book);
         void UpdateOneBook(Book book);
         void DeleteOneBook(Book book);
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Repostories.EFCore;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
 
 var app = builder.Build();
 

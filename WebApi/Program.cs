cad71cc0cb3d8ca943de4cd89b0ac4f9fc589e9d
Altug Aytimur <aytimuraltug@gmail.com>
2024-04-05 18:46:04 +0300

    Service Layer Created

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
+builder.Services.COnfigureServiceManager();
+
 
 var app = builder.Build();
 

d6b7a59aa3cb352cbd3441aff99ee7a0f9d98efb
Altug Aytimur <aytimuraltug@gmail.com>
2024-04-06 14:50:59 +0300

    NLog created

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -12,18 +12,16 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
 
-        public BookManager(IRepositoryManager manager)
+        public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
             _manager = manager;
+            _logger = logger;
         }
 
         public Book CreateOneBook(Book book)
         {
-            if (book is null)
-                throw new ArgumentNullException(nameof(book));
-
-
             _manager.Book.CreateOneBook(book);
             _manager.Save();
             return book;
@@ -35,8 +33,11 @@ namespace Services
             //check entity
             var entity = _manager.Book.GetOneBooksById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id:{id} could not found");
-
+            {
+                var message = $"The book with id:{id} could not found";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
         }
@@ -56,7 +57,12 @@ namespace Services
             //check entity
             var entity = _manager.Book.GetOneBooksById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id:{id} could not found.");
+            {
+                var message = $"Book with id:{id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
+               
 
             //check params
             if (book is null)
